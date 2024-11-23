9a8dc53ac8fbeaa22c26748d402695f5641b81cd
rohaizanr <rohaizanr@gmail.com>
2024-11-24 05:28:04 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -3,37 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const getSubdomain = () => {
-    const parts = window.location.hostname.split('.')
-    if (parts.length >= 3) {
-      return parts[0]
-    }
-    return null
-  }
-  
-  const subdomain = getSubdomain()
-  const dynamicSubdomain = subdomain === 'localhost' ? 'demo' : subdomain
-
   useEffect(() => {
-    const currentPath = window.location.pathname;
-    if (currentPath === '/' || currentPath === '') {
-      setTimeout(() => {
-        window.location.href = `https://${dynamicSubdomain}.e-masjid.my/web`;
-      }, 1000);
-    }
-  }, [dynamicSubdomain]);
+    const redirectTimeout = setTimeout(() => {
+      window.location.href = '/web';
+    }, 1500);
+
+    return () => clearTimeout(redirectTimeout); // Cleanup timeout on unmount
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {window.location.pathname === '/' || window.location.pathname === '' ? (
-            'Selamat datang'
-          ) : (
-            '404 - Not found'
-          )}
-        </p>
+        <p>Loading...</p>
       </header>
     </div>
   );
